bd497197c868301058fb87ca51c30d4647f2a6c7
Vi <vika.kr2001@gmail.com>
2023-07-25 19:06:38 +0300

    finished page

--- scripts/color_bg_change.js
+++ scripts/color_bg_change.js
@@ -3,17 +3,17 @@ window.addEventListener('scroll', () => {
         const html = document.querySelector('html')
         const body = document.querySelector('body')
         html.style.backgroundColor = `#ecdbd7`;
-        html.style.transition = `0.3s all`;
+        html.style.transition = `1s all`;
         body.style.backgroundColor = `#ecdbd7`;
-        body.style.transition = `0.3s all`;
+        body.style.transition = `1s all`;
     }
 
     if(window.scrollY < 1500) {
         const html = document.querySelector('html')
         const body = document.querySelector('body')
         html.style.backgroundColor = `#faf5f5`;
-        html.style.transition = `0.3s all`;
+        html.style.transition = `1s all`;
         body.style.backgroundColor = `#faf5f5`;
-        body.style.transition = `0.3s all`;
+        body.style.transition = `1s all`;
     }
 });
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,4 +1,9 @@
-var slideIndex = 2;
+if (window.innerWidth > 1200) {
+    var slideIndex = 2;
+}
+else{
+    var slideIndex = 1;
+}
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -12,11 +17,21 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("feedback__img");
-    if (n > slides.length) {slideIndex = 2}    
-    if (n < 2) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    if (window.innerWidth > 1200) {
+        if (n > slides.length) {slideIndex = 2}    
+        if (n < 2) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "initial";
+        slides[slideIndex-2].style.display = "initial";
     }
-    slides[slideIndex-1].style.display = "initial";
-    slides[slideIndex-2].style.display = "initial";  
+    else {
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "initial";
+    }
 }
